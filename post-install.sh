c71fc4b8f72f66405a12fb6009216fadc4341bd6
Brian Mayo <ioprotium@gmail.com>
2021-12-30 13:26:30 +0100

    added post-install script

--- post-install.sh
+++ post-install.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
+reset_color=$(tput sgr 0)
+
+info() {
+  printf "%s[*] %s%s\n" "$(tput setaf 2)" "$1" "$reset_color"
+}
+
 # Set of commands that need user interaction
-echo "----- autoenv post install -----"
-echo "Triggering nvim plugins install"
+info "======== Autoenv post install ========\n"
+info "Triggering nvim plugins install"
+
 nvim +PlugInstall +qall
 
-echo "Triggering Rust-up"
-rustup-init
+if ! hash rustc &>/dev/null; then
+  info "Triggering Rust-up"
+  rustup-init
+fi
 
-echo "----- Finished -----"
-echo "System must restart"
-sudo shutdown -r now
+info "Done"
+info "System must restart. Restart?"
 
+select yn in "y" "n"; do
+  case $yn in
+      y ) sudo shutdown -r now; break;;
+      n ) exit;;
+  esac
+done
 
